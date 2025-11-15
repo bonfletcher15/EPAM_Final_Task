ea72b6dff239229858d9ff17d01b918747b320b5
Serhii <fserg2003@gmail.com>
2025-11-16 01:22:47 +0200

    Completed 3 login tests

---
+++ src/tests/exam.tests.js
@@ -0,0 +1,41 @@
+describe('Login form testing', () => {
+    beforeEach(async () => {
+        await browser.url(`https://www.saucedemo.com/`);
+    })
+
+    it('Test Login form with empty credentials', async () => {
+
+        await $('#user-name').setValue('John Doe');
+        await $('#password').setValue('1234');
+
+        await $('#user-name').clearValue();
+        await $('#password').clearValue();
+        
+        await $('#login-button').click();
+        await expect($('h3')).toHaveText('Epic sadface: Username is required');
+    })
+
+    it('Test Login form with credentials by passing Username', async () => {
+
+        await $('#user-name').setValue('John Doe');
+        await $('#password').setValue('1234');
+
+        await $('#password').clearValue();
+
+        await $('#login-button').click();
+
+        await expect($('h3')).toHaveText('Epic sadface: Password is required');
+    })
+
+    it('Test Login form with credentials by passing Username & Password', async () => {
+
+        await $('#user-name').setValue('problem_user');
+
+        await $('#password').setValue('secret_sauce');
+
+        await $('#login-button').click();
+
+        await expect(browser).toHaveTitle('Swag Labs');
+    })
+
+})
